a6224ed27cc8e76d447b5623cb10285427525caf
fengrui <fengrui358@163.com>
2017-02-22 18:39:38 +0800

    暂时提交对树形控件的修改

---
+++ WpfLabs/ContactsTree.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfLabs.Models;
+
+namespace WpfLabs
+{
+    /// <summary>
+    /// ContactsTree.xaml 的交互逻辑
+    /// </summary>
+    public partial class ContactsTree : UserControl
+    {
+        public ContactsTree()
+        {
+            InitializeComponent();
+
+            InitSource();
+
+            DataContext = this;
+        }
+
+        public List<OrganizationModel> ContactsTreeSource { get; set; }
+
+        private void InitSource()
+        {
+            var mockDatas = new List<OrganizationModel>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                var organizationModel = new OrganizationModel { Icon = "Resources/Telephone.png", Name = "组织" + Guid.NewGuid() };
+
+                for (int j = 0; j < 2; j++)
+                {
+                    var childOrganizationModel = new OrganizationModel { Icon = "Resources/Telephone.png", Name = "组织" + Guid.NewGuid() };
+
+                    for (int k = 0; k < 2; k++)
+                    {
+                        var peopleModel = new PeopleModel();
+                        peopleModel.Name = "人员" + Guid.NewGuid();
+
+                        childOrganizationModel.PeopleChildren.Add(peopleModel);
+                    }
+
+                    organizationModel.OrganizationChildren.Add(childOrganizationModel);
+                }
+
+                mockDatas.Add(organizationModel);
+            }
+
+            ContactsTreeSource = mockDatas;
+        }
+    }
+}
---
+++ WpfLabs/Models/PeopleModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace WpfLabs.Models
+{
+    public class PeopleModel : ObservableObject
+    {
+        private long _id;
+        private string _name;
+
+        private long Id
+        {
+            get { return _id; }
+            set { Set(ref _id, value); }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(ref _name, value); }
+        }
+    }
+}
