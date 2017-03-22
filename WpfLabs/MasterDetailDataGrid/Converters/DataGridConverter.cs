e9060d2bc8294880598d5907f6c281f61b33307f
fengrui <fengrui358@163.com>
2017-03-22 16:58:26 +0800

    完善列表项的展开

---
+++ WpfLabs/MasterDetailDataGrid/Converters/DataGridConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfLabs.MasterDetailDataGrid.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class DataGridRowDetailVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //修改默认的展示子类容的方式
+            //目前的需求只能由Button来改变
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
