710f75a6d7dca1c03c1ed1600b7b336768a2351a
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2017-08-29 18:04:12 +0800

    暂时提交~

--- WpfLabs/WpfLabs/MeasureOverrideAndArrangeOverride/CustomerControls/MyControl.cs
+++ WpfLabs/WpfLabs/MeasureOverrideAndArrangeOverride/CustomerControls/MyControl.cs
@@ -1,8 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfLabs.MeasureOverrideAndArrangeOverride.CustomerControls
 {
     public class MyControl : Control
     {
+        protected override Size MeasureOverride(Size constraint)
+        {
+            Debug.WriteLine($"Name:{Name};constraintSize:{constraint}");
+
+            return base.MeasureOverride(constraint);
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            Debug.WriteLine($"Name:{Name};arrangeBoundsSize:{arrangeBounds}");
+
+            return base.ArrangeOverride(arrangeBounds);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawRectangle(Background, null, new Rect(new Size(100,100)));
+        }
     }
 }
--- WpfLabs/WpfLabs/MeasureOverrideAndArrangeOverride/CustomerPanels/MyPanel.cs
+++ WpfLabs/WpfLabs/MeasureOverrideAndArrangeOverride/CustomerPanels/MyPanel.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfLabs.MeasureOverrideAndArrangeOverride.CustomerPanels
 {
@@ -7,12 +9,21 @@ namespace WpfLabs.MeasureOverrideAndArrangeOverride.CustomerPanels
     {
         protected override Size MeasureOverride(Size availableSize)
         {
+            Debug.WriteLine($"Name:{Name};availableSize:{availableSize}");
+
             return base.MeasureOverride(availableSize);
         }
 
         protected override Size ArrangeOverride(Size finalSize)
         {
+            Debug.WriteLine($"Name:{Name};finalSize:{finalSize}");
+
             return base.ArrangeOverride(finalSize);
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
         }
     }
 }
