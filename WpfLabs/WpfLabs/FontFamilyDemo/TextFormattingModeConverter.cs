604be2681d4f154ac68a9998ab11fccb884f5a44
fengrui <qjdev@qq.com>
2017-11-30 22:18:05 +0800

    增加一个文本格式化显示转换器

---
+++ WpfLabs/WpfLabs/FontFamilyDemo/TextFormattingModeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfLabs.FontFamilyDemo
+{
+    [ValueConversion(typeof(double), typeof(TextFormattingMode))]
+    public class TextFormattingModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double)
+            {
+                if ((double) value >= 15)
+                {
+                    return TextFormattingMode.Ideal;
+                }
+                else
+                {
+                    return TextFormattingMode.Display;
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
