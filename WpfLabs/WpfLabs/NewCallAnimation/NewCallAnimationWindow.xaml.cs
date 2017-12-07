0938bb75d876bcd37734a8f8b692f167e76e180b
秦晋 <qjdev@qq.com>
2017-12-08 00:25:59 +0800

    模拟按钮点击

--- WpfLabs/WpfLabs/NewCallAnimation/NewCallAnimationWindow.xaml.cs
+++ WpfLabs/WpfLabs/NewCallAnimation/NewCallAnimationWindow.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,6 +22,9 @@ namespace WpfLabs.NewCallAnimation
     /// </summary>
     public partial class NewCallAnimationWindow : Window
     {
+        private Random _random = new Random();
+        private System.Threading.Timer _timer;
+
         public NewCallAnimationWindow()
         {
             InitializeComponent();
@@ -27,6 +33,25 @@ namespace WpfLabs.NewCallAnimation
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             RotateTimer.IsActive = !RotateTimer.IsActive;
+        }
+
+        private void NewCallAnimationWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _timer = new System.Threading.Timer(s =>
+            {
+                ControlBtn.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    ControlBtn.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+                }));
+
+                _timer.Change(TimeSpan.FromSeconds(_random.Next(10, 21)), Timeout.InfiniteTimeSpan);
+            }, null, TimeSpan.FromSeconds(_random.Next(10, 21)), Timeout.InfiniteTimeSpan);
+        }
+
+        private void NewCallAnimationWindow_OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            _timer = null;
         }
     }
 }
