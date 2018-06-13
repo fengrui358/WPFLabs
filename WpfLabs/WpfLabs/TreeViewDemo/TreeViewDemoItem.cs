b8c71965f68fed4affa96e641b6b5e22015a3c24
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2018-06-13 17:30:38 +0800

    树选中项问题，待测试

---
+++ WpfLabs/WpfLabs/TreeViewDemo/TreeViewDemoItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WpfLabs.TreeViewDemo
+{
+    public class TreeViewDemoItem
+    {
+        public string Name { get; set; }
+
+        public List<TreeViewDemoItem> Children { get; set; }
+    }
+}
---
+++ WpfLabs/WpfLabs/TreeViewDemo/TreeViewDemoWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfLabs.TreeViewDemo
+{
+    /// <summary>
+    /// TreeViewDemoWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class TreeViewDemoWindow : Window
+    {
+        public List<TreeViewDemoItem> TreeViewItems { get; set; }
+
+        public TreeViewDemoWindow()
+        {
+            InitializeComponent();
+
+            TreeViewItems = new List<TreeViewDemoItem>();
+            for (int i = 0; i < 5; i++)
+            {
+                var newTreeItem = new TreeViewDemoItem
+                {
+                    Name = Guid.NewGuid().ToString("N"),
+                    Children = new List<TreeViewDemoItem>()
+                };
+            }
+        }
+    }
+}
