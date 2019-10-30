805db65a3cd8b65cff4760b1d3f6a8ee4ba8542f
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2019-10-30 11:45:50 +0800

    测试Gif切换

--- WpfLabs/WpfLabs/GifShowDemo/GifShowDemoWindow.xaml.cs
+++ WpfLabs/WpfLabs/GifShowDemo/GifShowDemoWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using WpfAnimatedGif;
 
 namespace WpfLabs.GifShowDemo
@@ -14,12 +17,17 @@ namespace WpfLabs.GifShowDemo
         {
             InitializeComponent();
 
-            //ImageBehavior.SetRepeatBehavior(GifImage, new RepeatBehavior(1));
+            ImageBehavior.SetRepeatBehavior(GifImage, new RepeatBehavior(1));
         }
 
         private void ImageBehavior_OnAnimationCompleted(object sender, RoutedEventArgs e)
         {
             Debug.WriteLine("Gif animation completed.");
+            //ImageBehavior.SetAnimatedSource(GifImage2, new BitmapImage(new Uri("pack://application:,,,/WpfLabs;component/GifShowDemo/Test.gif")));
+            GifImage.Visibility = Visibility.Collapsed;
+            GifImage2.Visibility = Visibility.Visible;
+
+            ((Grid) GifImage.Parent).Children.Remove(GifImage);
         }
     }
 }
