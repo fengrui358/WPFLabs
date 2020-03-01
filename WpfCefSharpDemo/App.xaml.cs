a5ba183bf538f66b3813f295bde5a3816a63d716
free <devqj@qq.com>
2020-03-01 22:21:05 +0800

    commit

--- WpfCefSharpDemo/App.xaml.cs
+++ WpfCefSharpDemo/App.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
+using CefSharp;
+using CefSharp.Wpf;
 
 namespace WpfCefSharpDemo
 {
@@ -13,5 +18,53 @@ namespace WpfCefSharpDemo
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //Add Custom assembly resolver
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+
+            //Any CefSharp references have to be in another method with NonInlining
+            // attribute so the assembly rolver has time to do it's thing.
+            InitializeCefSharp();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void InitializeCefSharp()
+        {
+            //For Windows 7 and above, best to include relevant app.manifest entries as well
+            Cef.EnableHighDPISupport();
+
+            //We're going to manually call Cef.Shutdown below, this maybe required in some complex scenarios
+            CefSharpSettings.ShutdownOnExit = false;
+
+            var settings = new CefSettings();
+
+            // Set BrowserSubProcessPath based on app bitness at runtime
+            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                Environment.Is64BitProcess ? "x64" : "x86",
+                "CefSharp.BrowserSubprocess.exe");
+
+            // Make sure you set performDependencyCheck false
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+        }
+
+        // Will attempt to load missing assembly from either x86 or x64 subdir
+        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                    Environment.Is64BitProcess ? "x64" : "x86",
+                    assemblyName);
+
+                return File.Exists(archSpecificPath)
+                    ? Assembly.LoadFile(archSpecificPath)
+                    : null;
+            }
+
+            return null;
+        }
     }
 }
