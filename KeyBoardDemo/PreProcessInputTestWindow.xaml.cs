fb76d05bbf8f3a04fffdf0959df688766615bd76
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-03-03 15:18:03 +0800

    完善获取快捷键的方法

--- KeyBoardDemo/PreProcessInputTestWindow.xaml.cs
+++ KeyBoardDemo/PreProcessInputTestWindow.xaml.cs
@@ -45,12 +45,17 @@ namespace KeyBoardDemo
 
                         if (r == Keyboard.KeyDownEvent)
                         {
-                            InputTxt.Text += $"按下:{keyEvent?.Key} Modifiers:{k.KeyboardDevice.Modifiers}{Environment.NewLine}";
+                            InputTxt.Text +=
+                                $"按下:{keyEvent?.Key} Modifiers:{k.KeyboardDevice.Modifiers} SystemKey:{keyEvent?.SystemKey}{Environment.NewLine}";
+
+                            InputTxt.Text +=
+                                $"确定真实按键:{k.KeyboardDevice.Modifiers} {GetRealKey(keyEvent.Key, keyEvent.SystemKey)}{Environment.NewLine}";
                         }
 
                         if (r == Keyboard.KeyUpEvent)
                         {
-                            InputTxt.Text += $"抬起:{keyEvent?.Key} Modifiers:{k.KeyboardDevice.Modifiers}{Environment.NewLine}";
+                            InputTxt.Text +=
+                                $"抬起:{keyEvent?.Key} Modifiers:{k.KeyboardDevice.Modifiers} SystemKey:{keyEvent?.SystemKey}{Environment.NewLine}";
                         }
                     }
                 }
@@ -59,6 +64,25 @@ namespace KeyBoardDemo
             {
                 InputTxt.Text = ex.ToString();
             }
+        }
+
+        private Key GetRealKey(Key? key, Key? systemKey)
+        {
+            var result = Key.None;
+
+            if (key != null)
+            {
+                if (key == Key.System)
+                {
+                    result = systemKey ?? Key.None;
+                }
+                else
+                {
+                    result = key.Value;
+                }
+            }
+
+            return result;
         }
     }
 }
