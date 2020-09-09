bccc74195dc1e2082130800b71a708c3a2fa8d4b
fengrui@ceiec.com.cn <fengrui@ceiec.com.cn>
2020-09-09 13:32:20 +0800

    测试

---
+++ WpfLabs/WpfLabs/DependencyPropertyInheritsDemo/BottomInnerControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfLabs.DependencyPropertyInheritsDemo
+{
+    /// <summary>
+    /// BottomInnerControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class BottomInnerControl : UserControl
+    {
+        public static readonly DependencyProperty TestStringProperty = DependencyProperty.Register(
+            "TestString", typeof(string), typeof(BottomInnerControl), new PropertyMetadata(default(string)));
+
+        public string TestString
+        {
+            get => (string) GetValue(TestStringProperty);
+            set => SetValue(TestStringProperty, value);
+        }
+
+        public BottomInnerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
